c02b49197cd96ae23a9a763d8c589617ddce2dc7
urayusuke <crfnf690@yahoo.co.jp>
2020-06-30 20:21:47 +0900

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,6 +25,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
+    redirect_to article_path
   end
 
   def destroy
