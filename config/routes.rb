6c9a51bd449a925547787eb2eed6be59bdc58700
urayusuke <crfnf690@yahoo.co.jp>
2020-07-11 23:41:32 +0900

    delete devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config 
   ActiveAdmin.routes(self)  
-  devise_for :users
-  as :user do
-    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-    put 'users' => 'devise/registrations#update', :as => 'user_registration'
-  end
-  
+  devise_for :users  
   root "articles#index" 
   resources :articles
 end
